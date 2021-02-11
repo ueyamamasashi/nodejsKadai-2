62af9c87eff64b2cbace6e631e7ee46cb40af4c4
ueyamamasashi <ueyamaza@mctv.ne.jp>
2021-02-11 14:45:26 +0900

    second

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,13 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const controller = require('../controllers/controller');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+// router.get('/', function(req, res, next) {
+//   res.render('index', { title: 'Express' });
+// });
+
+router.get('/', controller.login);
+router.post('/', controller.loginPost);
 
 module.exports = router;
